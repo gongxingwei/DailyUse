75b6a88138e29289dea9a8f368ed769fb9f1d1f5
BakerSean <bakersean@foxmail.com>
2024-12-25 16:29:19 +0800

    20241225 icon

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,15 +1,15 @@
-import { app as o, BrowserWindow as l, ipcMain as a, dialog as _ } from "electron";
+import { app as o, BrowserWindow as c, ipcMain as a, dialog as _ } from "electron";
 import { fileURLToPath as h } from "node:url";
 import e from "node:path";
 import { promises as t } from "fs";
 const d = e.dirname(h(import.meta.url));
 process.env.APP_ROOT = e.join(d, "..");
-const s = process.env.VITE_DEV_SERVER_URL, E = e.join(process.env.APP_ROOT, "dist-electron"), p = e.join(process.env.APP_ROOT, "dist");
+const s = process.env.VITE_DEV_SERVER_URL, v = e.join(process.env.APP_ROOT, "dist-electron"), p = e.join(process.env.APP_ROOT, "dist");
 process.env.VITE_PUBLIC = s ? e.join(process.env.APP_ROOT, "public") : p;
 let r;
 function m() {
-  r = new l({
-    icon: e.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+  r = new c({
+    icon: e.join(process.env.VITE_PUBLIC, "DailyUse.svg"),
     webPreferences: {
       webSecurity: !1,
       preload: e.join(d, "preload.mjs")
@@ -24,34 +24,34 @@ o.on("window-all-closed", () => {
   process.platform !== "darwin" && (o.quit(), r = null);
 });
 o.on("activate", () => {
-  l.getAllWindows().length === 0 && m();
+  c.getAllWindows().length === 0 && m();
 });
 o.whenReady().then(m);
-a.handle("readFile", async (c, i) => {
+a.handle("readFile", async (l, n) => {
   try {
-    return await t.mkdir(e.dirname(i), { recursive: !0 }), await t.readFile(i, "utf8");
-  } catch (n) {
-    throw n.code === "ENOENT", n;
+    return await t.mkdir(e.dirname(n), { recursive: !0 }), await t.readFile(n, "utf8");
+  } catch (i) {
+    throw i.code === "ENOENT", i;
   }
 });
-a.handle("writeFile", async (c, i, n) => {
+a.handle("writeFile", async (l, n, i) => {
   try {
-    await t.mkdir(e.dirname(i), { recursive: !0 }), await t.writeFile(i, n, "utf8");
+    await t.mkdir(e.dirname(n), { recursive: !0 }), await t.writeFile(n, i, "utf8");
   } catch (w) {
     throw w;
   }
 });
-a.handle("selectFile", async (c, i) => {
-  const n = await _.showOpenDialog({
+a.handle("selectFile", async (l, n) => {
+  const i = await _.showOpenDialog({
     properties: ["openFile", "createDirectory"],
     filters: [
       { name: "Markdown", extensions: ["md"] }
     ]
   });
-  return n.canceled ? null : n.filePaths[0];
+  return i.canceled ? null : i.filePaths[0];
 });
 export {
-  E as MAIN_DIST,
+  v as MAIN_DIST,
   p as RENDERER_DIST,
   s as VITE_DEV_SERVER_URL
 };
