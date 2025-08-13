45c8358b4606bdcb64972da645f5880bb965eb31
BakerSean <bakersean@foxmail.com>
2025-08-13 08:14:43 +0800

    chore: rename

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -3,7 +3,6 @@ import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue';
 import router from './shared/router';
-import { api } from './plugins/axios';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 
@@ -15,12 +14,15 @@ pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia).use(router).use(vuetify);
 
-app.config.globalProperties.$api = api as any;
+// app.config.globalProperties.$api = api as any;
 
-declare module 'vue' {
-  interface ComponentCustomProperties {
-    $api: typeof api;
-  }
-}
+// declare module 'vue' {
+//   interface ComponentCustomProperties {
+//     $api: typeof api;
+//   }
+// }
 
-app.mount('#app');
+app.mount('#app')
+.$nextTick(() => { 
+  console.log('🚀 App mounted successfully');
+});
